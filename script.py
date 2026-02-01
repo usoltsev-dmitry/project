2f718f44094f973a484040bf472bf73d3e71b96e
Dmitry Usoltsev <usoltsev.d@citilink.ru>
2026-02-01 12:19:31 +0500

    Тестовое изменения скрипта

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import csv
-# Import data file
+# Import data file. Change in test branch
 with open("data.csv", newline="", encoding="utf-8") as f:
     reader = csv.DictReader(f)
     for row in reader:
